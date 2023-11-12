8c6efcafdde51baf1e40ee8e22d20f6fa364832b
ShkerbsKababik <110543788+ShkerbsKababik@users.noreply.github.com>
2023-11-12 17:26:10 +0300

    0.0.1
    *Project Created
    *Mvc Structure Added
    *Login and Register pages Created

---
+++ AspMvcChat/AspMvcChat/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspMvcChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string Register(string login, string password)
+        {        
+            return $"{login} {password}";
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string Login(string login, string password)
+        {
+            return $"{login} {password}";
+        }
+    }
+}
